c4d031212d2b8fcb3d36c37857edf18a4160f88d
dodytrifa <dody.trifa@gmail.com>
2021-04-15 14:38:34 +0700

    export pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import {About, AuthWrapper, Cart, Checkout, Error, Home, PrivateRoute,Products,SingleProduct} from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
