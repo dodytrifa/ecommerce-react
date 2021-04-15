6ce897f2ac708a2e58d9e18cb677402bfeda8b4f
dodytrifa <dody.trifa@gmail.com>
2021-04-15 15:33:17 +0700

    add footer

--- src/App.js
+++ src/App.js
@@ -6,11 +6,30 @@ import {About, AuthWrapper, Cart, Checkout, Error, Home, PrivateRoute,Products,S
 function App() {
   return (
     <Router>
+      <Navbar />
+      <Sidebar />
       <Switch>
         <Route exact path='/'>
           <Home />
         </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct />} />
+        <Route exact path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route exact path='*'>
+          <Error />
+        </Route>
       </Switch>
+        <Footer />
     </Router>
   )
 }
